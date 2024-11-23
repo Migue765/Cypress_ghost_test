232967fd7948ec461173a552dc60fff9530cb671
JUANES5545 <juanes.reich@gmail.com>
2024-11-23 17:04:21 -0500

    Full FK test to tag_management v2

--- cypress/cypress/e2e/tag_management/E040-FK-edit_tag_facebook.cy.js
+++ cypress/cypress/e2e/tag_management/E040-FK-edit_tag_facebook.cy.js
@@ -1,6 +1,6 @@
 import {faker} from '@faker-js/faker';
 
-describe('Edit an existing tag with invalid slug', () => {
+describe('Edit an existing tag with invalid slug and update Facebook card', () => {
     const LOCAL_HOST = Cypress.env('LOCAL_HOST');
 
     beforeEach("Precondition: Admin login", () => {
@@ -8,11 +8,8 @@ describe('Edit an existing tag with invalid slug', () => {
         cy.createTags();
     });
 
-    it('Edit an existing tag and save with an invalid slug', () => {
-        cy.visit(LOCAL_HOST + "#/dashboard");
-        cy.wait(4000);
-
-        cy.get('a[data-test-nav="tags"]').click();
+    it('Edit an existing tag, save with an invalid slug, and update Facebook card', () => {
+        cy.visit(LOCAL_HOST + "#/tags");
         cy.wait(2000);
         cy.get('section.view-container.content-list').find('a[title="Edit tag"]').first().click();
         cy.wait(2000);
@@ -20,9 +17,11 @@ describe('Edit an existing tag with invalid slug', () => {
         cy.get('input[data-test-input="tag-name"]').clear().type(faker.lorem.word());
         cy.get('input[data-test-input="tag-slug"]').clear().type('invalid_slug!@#'); // Slug inválido
 
-        cy.get('button.gh-btn.gh-btn-expand').first().click();
-        cy.get('input#meta-title').clear().type('Meta Title with Invalid Slug'); // Meta title válido
-        cy.wait(2000);
+        cy.get('div.gh-expandable-block').eq(2).within(() => {
+            cy.get('button.gh-btn-expand').click();
+            cy.get('input#og-title').clear().type(faker.lorem.words(20).substring(0, 100));
+            cy.get('p').contains("You’ve used 100").should('exist'); // Verify the character count
+        });
 
         cy.get('span[data-test-task-button-state="idle"]').click();
         cy.wait(2000);
@@ -33,4 +32,7 @@ describe('Edit an existing tag with invalid slug', () => {
         cy.contains('invalid_slug!@#').should('not.exist'); // Verifica que no exista el slug inválido
     });
 
+    it('Delete all tags and verify they are not in the tag list', () => {
+        cy.deleteAllTags();
+    });
 });
