2f6bd6c4a8b06dd56a96cfc8cc78fef2907d4802
JUANES5545 <juanes.reich@gmail.com>
2024-11-23 00:43:32 -0500

    blue adjustments

--- cypress/cypress/e2e/access-profile-user/Faker-Cypress-acces-profile-user.cy.js
+++ cypress/cypress/e2e/access-profile-user/Faker-Cypress-acces-profile-user.cy.js
@@ -21,7 +21,7 @@ describe('Access profile: View error update website', () => {
         // Return false to prevent Cypress from failing the test
         return false;
     });
-  
+
 
      it('I Should slug in setting profile', () => {
         const namefaker = faker.person.fullName();
@@ -38,35 +38,35 @@ describe('Access profile: View error update website', () => {
         cy.get('a[data-test-nav="user-profile"').click();
         cy.wait(2000);
 
-        cy.contains('label', 'Full name').parent().find('input').clear().type(namefaker); 
-        cy.contains('label', 'Full name').parent().find('input').should('have.value', namefaker);  
+        cy.contains('label', 'Full name').parent().find('input').clear().type(namefaker);
+        cy.contains('label', 'Full name').parent().find('input').should('have.value', namefaker);
 
-        cy.contains('label', 'Location').parent().find('input').clear().type(locationfaker); 
-        cy.contains('label', 'Location').parent().find('input').should('have.value', locationfaker);  
+        cy.contains('label', 'Location').parent().find('input').clear().type(locationfaker);
+        cy.contains('label', 'Location').parent().find('input').should('have.value', locationfaker);
 
-        cy.contains('label', 'Website').parent().find('input').clear().type(website); 
-        cy.contains('label', 'Website').parent().find('input').should('have.value', website);  
+        cy.contains('label', 'Website').parent().find('input').clear().type(website);
+        cy.contains('label', 'Website').parent().find('input').should('have.value', website);
 
-        cy.contains('label', 'Slug').parent().find('input').clear().type(slugname); 
-        cy.contains('label', 'Slug').parent().find('input').should('have.value', slugname);  
+        cy.contains('label', 'Slug').parent().find('input').clear().type(slugname);
+        cy.contains('label', 'Slug').parent().find('input').should('have.value', slugname);
 
-        cy.contains('label', 'Facebook profile').parent().find('input').clear().type(facebookprofile); 
-        cy.contains('label', 'Facebook profile').parent().find('input').should('have.value', facebookprofile);  
+        cy.contains('label', 'Facebook profile').parent().find('input').clear().type(facebookprofile);
+        cy.contains('label', 'Facebook profile').parent().find('input').should('have.value', facebookprofile);
 
-        cy.contains('label', 'X (formerly Twitter) profile').parent().find('input').clear().type(xprofile); 
-        cy.contains('label', 'X (formerly Twitter) profile').parent().find('input').should('have.value', xprofile);  
+        cy.contains('label', 'X (formerly Twitter) profile').parent().find('input').clear().type(xprofile);
+        cy.contains('label', 'X (formerly Twitter) profile').parent().find('input').should('have.value', xprofile);
 
-        cy.contains('label', 'Bio').parent().find('textarea').clear().type(bio); 
-        cy.contains('label', 'Bio').parent().find('textarea').should('have.value', bio);  
+        cy.contains('label', 'Bio').parent().find('textarea').clear().type(bio);
+        cy.contains('label', 'Bio').parent().find('textarea').should('have.value', bio);
 
 
         cy.get('button').contains('Save').click({force: true});
-        cy.wait(500); 
-        cy.contains('strong', 'Owner').should('be.visible').click();                  
-        cy.contains('label', 'Slug').parent().find('input').should('have.value', slugname);  
+        cy.wait(500);
+        cy.contains('strong', 'Owner').should('be.visible').click();
+        cy.contains('label', 'Slug').parent().find('input').should('have.value', slugname);
       });
- 
-       
+
+
       it('I should see error update more 200 caracters', () => {
 
         const bio1 = faker.lorem.words(100);
@@ -77,8 +77,8 @@ describe('Access profile: View error update website', () => {
         cy.get('a[data-test-nav="user-profile"').click();
         cy.wait(2000);
 
-        cy.contains('label', 'Bio').parent().find('textarea').clear().type(bio1); 
-        cy.contains('label', 'Bio').parent().find('textarea').should('have.value', bio1);  
+        cy.contains('label', 'Bio').parent().find('textarea').clear().type(bio1);
+        cy.contains('label', 'Bio').parent().find('textarea').should('have.value', bio1);
 
         cy.get('button').contains('Save').click({force: true});
         cy.get('span.mt-1.inline-block.text-xs.text-red.dark\\:text-red-500.order-3').should('have.text', 'Bio is too long');
@@ -97,11 +97,11 @@ describe('Access profile: View error update website', () => {
         cy.wait(2000);
 
 
-        cy.contains('label', 'X (formerly Twitter) profile').parent().find('input').clear().type(xprofile); 
-        cy.contains('label', 'X (formerly Twitter) profile').parent().find('input').should('have.value', xprofile);  
+        cy.contains('label', 'X (formerly Twitter) profile').parent().find('input').clear().type(xprofile);
+        cy.contains('label', 'X (formerly Twitter) profile').parent().find('input').should('have.value', xprofile);
 
-        cy.contains('label', 'Bio').parent().find('textarea').clear().type(Bio); 
-        cy.contains('label', 'Bio').parent().find('textarea').should('have.value', Bio);  
+        cy.contains('label', 'Bio').parent().find('textarea').clear().type(Bio);
+        cy.contains('label', 'Bio').parent().find('textarea').should('have.value', Bio);
 
         cy.get('span.mt-1.inline-block.text-xs.text-red.dark\\:text-red-500.order-3').should('have.text', 'Your Username is not a valid Twitter Username');
 
@@ -118,14 +118,14 @@ describe('Access profile: View error update website', () => {
         cy.wait(2000);
 
 
-        cy.contains('label', 'Email').parent().find('input').clear().type(email); 
-        cy.contains('label', 'Email').parent().find('input').should('have.value', email);  
+        cy.contains('label', 'Email').parent().find('input').clear().type(email);
+        cy.contains('label', 'Email').parent().find('input').should('have.value', email);
 
         cy.get('button').contains('Save').click({force: true});
         cy.get('span.mt-1.inline-block.text-xs.text-red.dark\\:text-red-500.order-3').should('have.text', 'Enter a valid email address');
 
       });
-    
+
       it('I should update a name and view the name', () => {
 
         const name = faker.person.fullName();
@@ -137,8 +137,8 @@ describe('Access profile: View error update website', () => {
         cy.wait(2000);
 
 
-        cy.contains('label', 'Full name').parent().find('input').clear().type(name); 
-        cy.contains('label', 'Full name').parent().find('input').should('have.value', name);  
+        cy.contains('label', 'Full name').parent().find('input').clear().type(name);
+        cy.contains('label', 'Full name').parent().find('input').should('have.value', name);
 
         cy.get('button').contains('Save').click({force: true});
         cy.get('h1.break-words.md\\:break-normal.text-white.md\\:text-4xl.leading-tighter').scrollIntoView({ behavior: 'smooth', block: 'center' }).should('be.visible').click();
@@ -157,24 +157,24 @@ describe('Access profile: View error update website', () => {
         cy.get('a[data-test-nav="user-profile"').click();
         cy.wait(2000);
 
-        
+
         cy.get('button').contains('Change password').click({force: true});
 
-        cy.contains('label', 'Old password').parent().find('input').clear().type(password); 
-        cy.contains('label', 'Old password').parent().find('input').should('have.value', password);  
+        cy.contains('label', 'Old password').parent().find('input').clear().type(password);
+        cy.contains('label', 'Old password').parent().find('input').should('have.value', password);
 
-        cy.contains('label', 'New password').parent().find('input').clear().type(passwordnew); 
-        cy.contains('label', 'New password').parent().find('input').should('have.value', passwordnew);  
+        cy.contains('label', 'New password').parent().find('input').clear().type(passwordnew);
+        cy.contains('label', 'New password').parent().find('input').should('have.value', passwordnew);
 
-        cy.contains('label', 'Verify password').parent().find('input').clear().type(passwordRepit); 
-        cy.contains('label', 'Verify password').parent().find('input').should('have.value', passwordRepit);  
-      
+        cy.contains('label', 'Verify password').parent().find('input').clear().type(passwordRepit);
+        cy.contains('label', 'Verify password').parent().find('input').should('have.value', passwordRepit);
+
         cy.get('button').contains('Change password').click({force: true});
         cy.get('span.mt-1.inline-block.text-xs.text-red.dark\\:text-red-500.order-3').should('have.text', 'Your new passwords do not matchYour new passwords do not match');
 
-        
+
       });
- 
+
       it('I Should image in profile ', () => {
         // Generar una URL de imagen aleatoria con Faker (usando Picsum)
         const imagenFalsaUrl = `https://picsum.photos/200/200?random=${faker.number.bigInt(100)}`;
@@ -192,15 +192,15 @@ describe('Access profile: View error update website', () => {
         // Usamos un archivo físico en lugar de una URL externa
         cy.get('#avatar').attachFile('picture.jpg'); // 'perfil.jpg' está en la carpeta fixtures
 
-        cy.get('#avatar').should('exist').and('have.prop', 'tagName', 'IMG').and('be.visible'); 
+        cy.get('#avatar').should('exist').and('have.prop', 'tagName', 'IMG').and('be.visible');
       });
 
 
       it('I should view error old different password', () => {
 
-        const passwoldnew = faker.string.alpha(10) 
-        const password = faker.string.alpha(15) 
-    
+        const passwoldnew = faker.string.alpha(10)
+        const password = faker.string.alpha(15)
+
         cy.visit(LOCAL_HOST + "#/dashboard");
         cy.wait(2000);
         cy.get('div.gh-user-avatar.relative').click();
@@ -208,44 +208,44 @@ describe('Access profile: View error update website', () => {
         cy.wait(2000);
 
 
-        
+
         cy.get('button').contains('Change password').click({force: true});
-    
-        cy.contains('label', 'Old password').parent().find('input').clear().type(passwoldnew); 
-        cy.contains('label', 'Old password').parent().find('input').should('have.value', passwoldnew);  
-    
-        cy.contains('label', 'New password').parent().find('input').clear().type(password); 
-        cy.contains('label', 'New password').parent().find('input').should('have.value', password);  
-    
-        cy.contains('label', 'Verify password').parent().find('input').clear().type(password); 
-        cy.contains('label', 'Verify password').parent().find('input').should('have.value', password);  
-      
+
+        cy.contains('label', 'Old password').parent().find('input').clear().type(passwoldnew);
+        cy.contains('label', 'Old password').parent().find('input').should('have.value', passwoldnew);
+
+        cy.contains('label', 'New password').parent().find('input').clear().type(password);
+        cy.contains('label', 'New password').parent().find('input').should('have.value', password);
+
+        cy.contains('label', 'Verify password').parent().find('input').clear().type(password);
+        cy.contains('label', 'Verify password').parent().find('input').should('have.value', password);
+
         cy.get('button').contains('Change password').click({force: true});
 
         cy.get('[data-testid="toast-error"]') // Selector para el toast de error
         .should('be.visible') // Verifica que el toast sea visible
-        .and('contain.text', 'Your password is incorrect.');   
+        .and('contain.text', 'Your password is incorrect.');
       });
-  
-  
+
+
       it('I should error in invalid  website', () => {
 
         const website = faker.number.romanNumeral();
-    
+
         cy.visit(LOCAL_HOST + "#/dashboard");
         cy.wait(2000);
         cy.get('div.gh-user-avatar.relative').click();
         cy.get('a[data-test-nav="user-profile"').click();
         cy.wait(2000);
-    
-    
-        cy.contains('label', 'Website').parent().find('input').clear().type(website); 
-        cy.contains('label', 'Website').parent().find('input').should('have.value', website);  
-    
+
+
+        cy.contains('label', 'Website').parent().find('input').clear().type(website);
+        cy.contains('label', 'Website').parent().find('input').should('have.value', website);
+
         cy.get('button').contains('Save').click({force: true});
         cy.get('span.mt-1.inline-block.text-xs.text-red.dark\\:text-red-500.order-3').should('have.text', 'Enter a valid URL');
-    
+
       });
-    
+
 
 });
