9ecca09ffe0f45a71e6b8180f5aabc29c98a684d
MIGUE765 <miguelalarcon1999@hotmail.com>
2024-11-24 19:55:25 -0500

    delete test members

--- cypress/cypress/e2e/member_management/E026-FK-delete_member.cy.js
+++ cypress/cypress/e2e/member_management/E026-FK-delete_member.cy.js
@@ -1,7 +1,9 @@
-describe('Member Management: Edit and Save Member Information', () => {
+const { faker } = require("@faker-js/faker");
+
+describe('Member Management: Add and Verify Member', () => {
 
     const LOCAL_HOST = Cypress.env('LOCAL_HOST');
-    const SCREENSHOT_PATH = 'E007-edit_member_before/edit_member';
+    const SCREENSHOT_PATH = 'E006-add_new_member_before/create_member';
     let screenshotCounter = 1;
 
     function takeScreenshot() {
@@ -19,36 +21,44 @@ describe('Member Management: Edit and Save Member Information', () => {
         return false;
     });
 
-    it('Edit an existing member\'s information and save the changes', () => {
+    it('Add a new member and verify it appears in the list of members', () => {
 
         cy.visit(LOCAL_HOST + "#/dashboard");
-        cy.wait(2000);
-
-        // Verify that the user has logged in successfully
-        cy.url().should('include', '/ghost/#/dashboard');
+        cy.wait(4000);
 
         // Enter the members section
         cy.get('[data-test-nav="members"]').click();
         cy.url().should('include', '/ghost/#/members');
 
-
-        // Create a new page
-        cy.get('.gh-members-list-row').first().click();
+        // Create a new member
+        cy.get('[data-test-new-member-button]').click();
         cy.wait(2000);
-        cy.url().should('include', '/ghost/#/members');
+        cy.url().should('include', '/ghost/#/members/new');
 
-        cy.get('[data-test-input="member-name"]').clear();
-        cy.get('[data-test-input="member-name"]').type('Miguel Gómez');
-        cy.get('[data-test-input="member-email"]').clear();
-        cy.get('[data-test-input="member-email"]').type('test2a25@test.com');
-        cy.get('.ember-power-select-trigger-multiple-input').type('Test Label 2{enter}');
-        cy.get('[data-test-input="member-note"]').type('Edited Note');
-
+        let name = faker.name.firstName() + ' ' + faker.name.lastName();
+        let email = faker.internet.email();
+        let note = faker.lorem.sentence();
+        let label = faker.animal.cat();
+        cy.get('[data-test-input="member-name"]').type(name);
+        cy.get('[data-test-input="member-email"]').type(email);
+        cy.get('.ember-power-select-trigger-multiple-input').type(label);
+        cy.get('[data-test-input="member-note"]').type(note);
 
         cy.get('[data-test-button="save"]').click();
 
+        cy.get('[data-test-nav="members"]').click();
 
-        cy.url().should('include', '/ghost/#/members');
+        cy.get('[data-test-list="members-list-item"]').first().invoke('text').should('include', name);
 
+        //Delete member
+        cy.get('[data-test-list="members-list-item"]').each(($el, index, $list) => {
+            cy.get('[data-test-list="members-list-item"]').first().click();
+            cy.get('[data-test-button="member-actions"]').click();
+            cy.get('[data-test-button="delete-member"').click();
+            cy.get('[data-test-button="confirm"]').click();
+            cy.wait(1000);
+        });
+
+        cy.get('[data-test-list="members-list-item"]').first().invoke('text').should('not.include', name);
     });
 });
