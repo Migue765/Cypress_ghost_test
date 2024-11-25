fff2c93b8030886e50273387c7eb747f851faa50
Jazmin Natalia Cordoba <jn.cordobap1@uniandes.edu.co>
2024-11-24 22:17:55 -0500

    E080-90

--- cypress/cypress/e2e/settings_management/E082-AP-update_tiers_long_title_general.cy.js
+++ cypress/cypress/e2e/settings_management/E082-AP-update_tiers_long_title_general.cy.js
@@ -21,27 +21,34 @@ describe('Modify Site Name', () => {
         // Scenarios with Faker.js
         let random_pos= mockData[Math.floor(Math.random() * mockData.length)];
         const scenarios = [
-            { description: 'url ', data: random_pos.complete_url, valid: true},
-
+            { description: 'long', data: random_pos.partial_url, valid: false },
         ];
+        
 
         scenarios.forEach((scenario, index) => {
-            cy.get('#admin-x-settings-scroller > div > div:nth-child(1) > div > div:nth-child(7) > div.flex.items-start.justify-between.gap-4 > div:nth-child(2) > div > button').click();
+            cy.get('div[data-testid="tier-card"][data-tier="free"]').click();
             cy.log(`Scenario ${index + 1}: ${scenario.description}`);
-            cy.get('input[placeholder="https://x.com/ghost"]').clear();
+            cy.get('input[placeholder="Free').clear();
 
             // Type the data
-            cy.get('input[placeholder="https://x.com/ghost"]').type(scenario.data);
+            cy.get('input[placeholder="Free').type(scenario.data);
 
-            // Cancel the settings
-            cy.contains('button', 'Cancel').click();
+            // Save the settings
+            cy.contains('button', 'Save')
+            .should('be.visible') 
+            .click();       
+            cy.contains('button', 'Close')
+            .should('be.visible') 
+            .click();        
+
+
+            
             // Validate the result
             if (scenario.valid) {
                 cy.wait(4000);
-                cy.get('div.flex.items-center.mt-1')
-                .eq(4) 
-                .should('contain.text', scenario.data);
-                takeScreenshot();
+                cy.get('div.text-\\[1\\.65rem\\]')
+                .should('have.text', scenario.data);
+
             } else {
                 console.log('no se evidencia error o aivso');
                 takeScreenshot();
