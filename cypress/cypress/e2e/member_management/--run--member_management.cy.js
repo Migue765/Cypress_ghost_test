0bd7c030bd588331409306ca4b9416b947895d27
JUANES5545 <juanes.reich@gmail.com>
2024-11-24 22:38:49 -0500

    --run-- all Adjusted v2

--- cypress/cypress/e2e/member_management/--run--member_management.cy.js
+++ cypress/cypress/e2e/member_management/--run--member_management.cy.js
@@ -3,5 +3,5 @@ const requireTestFiles = require.context('../member_management', true, /\.cy\.js
 requireTestFiles.keys().forEach(requireTestFiles);
 
 describe('Run All Scenarios', () => {
-    // All tests in the tag_management folder will be executed
+    // All tests in the member_management folder will be executed
 });
